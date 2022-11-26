2a70609de636cdbc2dc610aa257a339d448241e3
gopi <mgopi1294@gmail.com>
2022-11-26 15:11:10 +0530

    rds added

---
+++ modules/rds/main.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "rds_db" {
+  allocated_storage    = var.rds_allocated_storage
+  db_name              = var.rds_db_name
+  engine               = var.rds_engine
+  engine_version       = var.rds_engine_version
+  instance_class       = var.rds_instance_class
+  username             = var.rds_username
+  password             = var.rds_password
+  parameter_group_name = var.rds_parameter_group_name
+  skip_final_snapshot  = var.rds_skip_final_snapshot
+}
---
+++ modules/rds/vars.tf
@@ -0,0 +1,9 @@
+variable "rds_allocated_storage" {}
+variable "rds_db_name" {}
+variable "rds_engine" {}
+variable "rds_engine_version" {}
+variable "rds_instance_class" {}
+variable "rds_username" {}
+variable "rds_password" {}
+variable "rds_parameter_group_name" {}
+variable "rds_skip_final_snapshot" {}
